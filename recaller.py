e79770130190de29c239380dfd4c999995195d31
Christopher Hernandez <10schris2005@gmail.com>
2026-06-24 16:22:28 +0000

    i think done with database stuff

--- recaller.py
+++ recaller.py
@@ -3,6 +3,8 @@ import pandas as pd
 import sqlalchemy as db
 
 url = "https://api.fda.gov/food/enforcement.json"
+engine = db.create_engine('sqlite:///food_status.db')
+
 
 def check_food(food):
   response = requests.get(url, params={"search": f"product_description:{food}", "limit": 1})
@@ -23,12 +25,35 @@ def check_food(food):
   })
 
   food_statistics = pd.DataFrame.from_dict(food_stats)
-  engine = db.create_engine('sqlite:///food_status.db')
-  food_statistics.to_sql('food_status', con=engine, if_exists='replace', index=False)
 
-def show_foods()
   with engine.connect() as connection:
-    query_result = connection.execute(db.text("SELECT * FROM food_status;")).fetchall()
-    print(pd.DataFrame(query_result))
+    connection.execute(db.text("""
+        CREATE TABLE IF NOT EXISTS food_status (
+            Food TEXT,
+            "Reason for recall" TEXT,
+            "Recall date" TEXT,
+            Status TEXT
+        )
+    """))
+
+    connection.execute(db.text("DELETE FROM food_status WHERE Food = :food"), {"food": item.get("product_description", "N/A")})
+    connection.commit()
+  
+  food_statistics.to_sql('food_status', con=engine, if_exists='append', index=False)
+
+def update_db():
+  with engine.connect() as connection:
+    query_result = connection.execute(db.text("SELECT Food FROM food_status;")).fetchall()
+  
+  foods = [row[0] for row in query_result]
+
+  for food in foods:
+    check_food(food)
+
+def delete_food(food):
+  with engine.connect() as connection:
+    connection.execute(db.text("DELETE FROM food_status WHERE Food = :food"), {"food": food})
+    connection.commit()
+  
 
   
