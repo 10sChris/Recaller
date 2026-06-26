00f839100abf6f808b3e07081d2f03d78c76bedb
hhiamte <157152785+hhiamte@users.noreply.github.com>
2026-06-26 13:51:14 +0100

    removed gemini api tests

--- test_recaller.py
+++ test_recaller.py
@@ -1,19 +1,14 @@
 import unittest
-import recaller
+import os 
 
+os.environ["GENAI_KEY"] = "fake-key" 
+
+import recaller
 
 # unit test function
 class TestRecaller(unittest.TestCase):
 
-    def test_recall_summary(self):
-        item = {
-            "product_description": "Test",
-            "reason_for_recall": "possible contamination"
-        }
-
-        res = recaller.summarize_recall(item)
-
-        self.assertIsNotNone(res)
+ 
 
     # Checking no results
     def test_check_food_no_results(self):
@@ -27,16 +22,6 @@ class TestRecaller(unittest.TestCase):
 
         self.assertIsNone(res)
 
-    # making sure it has text
-    def test_has_text(self):
-        item = {
-            "product_description": "Apple Juice",
-            "reason_for_recall": "undeclared ingredient"
-        }
-
-        res = recaller.summarize_recall(item)
-
-        self.assertTrue(len(res) > 0)
 
     # check random food
     def test_check_food_random_food(self):
