14cc90d85723cec3c79ada1ab4aa8506c8411d85
hhiamte <157152785+hhiamte@users.noreply.github.com>
2026-06-26 13:34:39 +0100

    fixed style problems

--- test_recaller.py
+++ test_recaller.py
@@ -1,50 +1,49 @@
-import unittest 
+import unittest
+import recaller
 
-import recaller 
 
-
-#Unit test function
+# unit test function
 class TestRecaller(unittest.TestCase):
-  def test_recall_summary(self):
-    item = {
-      "product_description": "Test",
-      "reason_for_recall": "possible contamination"    
-      }
 
-    res = recaller.summarize_recall(item)
+    def test_recall_summary(self):
+        item = {
+            "product_description": "Test",
+            "reason_for_recall": "possible contamination"
+        }
 
-    self.assertIsNotNone(res)
+        res = recaller.summarize_recall(item)
 
-  #Checking no results
-  def test_check_food_no_results(self):
-    res = recaller.check_food("chicken", summarize=False)
+        self.assertIsNotNone(res)
 
-    self.assertIsNone(res)
+    # Checking no results
+    def test_check_food_no_results(self):
+        res = recaller.check_food("chicken", summarize=False)
 
-  #testing the delete food feature
-  def test_delete_food(self):
-    res = recaller.delete_food(1)
+        self.assertIsNone(res)
 
-    self.assertIsNone(res)
-  
-  #making sure it has text
-  def test_has_text(self):
-    item = {
-      "product_description": "Apple Juice", 
-      "reason_for_recall": "undeclared ingredient"
-    }
+    # testing the delete food feature
+    def test_delete_food(self):
+        res = recaller.delete_food(1)
 
-    res = recaller.summarize_recall(item)
+        self.assertIsNone(res)
 
-    self.assertTrue(len(res) > 0)
-  
-  #check random food
-  def test_check_food_random_food(self):
-    res = recaller.check_food("asdfighjkl", summarize=False)
+    # making sure it has text
+    def test_has_text(self):
+        item = {
+            "product_description": "Apple Juice",
+            "reason_for_recall": "undeclared ingredient"
+        }
 
-    self.assertIsNone(res) 
+        res = recaller.summarize_recall(item)
 
-    
+        self.assertTrue(len(res) > 0)
+
+    # check random food
+    def test_check_food_random_food(self):
+        res = recaller.check_food("asdfghjkl", summarize=False)
+
+        self.assertIsNone(res)
+
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
